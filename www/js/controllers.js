01e1e61b26ba716d80fe0554f89d6cd3cf0453da
Abhay Amin <pixoloproductions@gmail.com>
2015-04-12 16:50:49 +0530

    dasf

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,9 +1,16 @@
 angular.module('starter.controllers', [])
 
+var input = "";
+
 .controller('DashCtrl', function ($scope) {
 
 
     $scope.number = "";
+    
+    $scope.change = function()
+    {
+        input = $scope.number;
+    };
 
 
 
@@ -16,7 +23,18 @@ angular.module('starter.controllers', [])
 
 .controller('ChatsCtrl', function ($scope, Chats) {
     $scope.array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-    $scope.magic = function () {}
+
+    $scope.jyoti = [];
+    for (var q = 0; q < $scope.array.length; q++) {
+        $scope.jyoti[q] = false;
+    };
+    var check = 0;
+    $scope.magic = function (i) {
+        check++;
+        if (check < 6) {
+            $scope.jyoti[i] = true;
+        };
+    }
 })
 
 .controller('ChatDetailCtrl', function ($scope, $stateParams, Chats) {
