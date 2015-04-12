dec7d57ffd3995f5f962d5e54d90cddbdb68bb21
Abhay Amin <pixoloproductions@gmail.com>
2015-04-12 12:58:05 +0530

    array split

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,27 +1,40 @@
 angular.module('starter.controllers', [])
 
-.controller('DashCtrl', function($scope) {
+.controller('DashCtrl', function ($scope) {
 
-    $scope.array1 = [1,2,3,4,5,6,7,8,9,10];
+    $scope.array1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+    $scope.array2 = [];
     
-    
+    for (var q = 0; q < $scope.array1.length; q++) {
+        var arr = [];
+        for (var w = 0; w < 3; w++) {
+            if ($scope.array1[q]) {
+                arr.push($scope.array1[q]);
+            };
+            if (w != 2) {
+                q++;
+            };
+        };
+        $scope.array2.push(arr);
+    };
+    console.log($scope.array2);
     $scope.text = "abc";
-    
+
 })
 
-.controller('ChatsCtrl', function($scope, Chats) {
-  $scope.chats = Chats.all();
-  $scope.remove = function(chat) {
-    Chats.remove(chat);
-  }
+.controller('ChatsCtrl', function ($scope, Chats) {
+    $scope.chats = Chats.all();
+    $scope.remove = function (chat) {
+        Chats.remove(chat);
+    }
 })
 
-.controller('ChatDetailCtrl', function($scope, $stateParams, Chats) {
-  $scope.chat = Chats.get($stateParams.chatId);
+.controller('ChatDetailCtrl', function ($scope, $stateParams, Chats) {
+    $scope.chat = Chats.get($stateParams.chatId);
 })
 
-.controller('AccountCtrl', function($scope) {
-  $scope.settings = {
-    enableFriends: true
-  };
+.controller('AccountCtrl', function ($scope) {
+    $scope.settings = {
+        enableFriends: true
+    };
 });
