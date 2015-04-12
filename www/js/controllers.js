7f2bd8cad67d78afb03d1c8a0edaffd8c2f82ed6
Abhay Amin <pixoloproductions@gmail.com>
2015-04-12 16:13:25 +0530

    ng-reapeat

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -2,28 +2,21 @@ angular.module('starter.controllers', [])
 
 .controller('DashCtrl', function ($scope) {
 
-    $scope.array1 = [0,1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-    $scope.array2 = [];
+  
+    $scope.number="";
     
-    console.log($scope.array1[11]);
-    for (var q = 0; q < $scope.array1.length; q++) {
-        var arr = [];
-        for (var w = 0; w < 3; w++) {
-            if ($scope.array1[q] || $scope.array1[q]==0) {
-                arr.push($scope.array1[q]);
-            };
-            if (w != 2) {
-                q++;
-            };
-        };
-        $scope.array2.push(arr);
-    };
-    console.log($scope.array2);
-    $scope.text = "abc";
-
+    
+    
+    
+    
+    
+    
+    
 })
 
 .controller('ChatsCtrl', function ($scope, Chats) {
+    $scope.array=[1,2,3,4,5,6,7,8,9,10];
+    
     $scope.chats = Chats.all();
     $scope.remove = function (chat) {
         Chats.remove(chat);
