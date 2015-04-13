660cbd36d2561f8b251c8f589399a40b5b540363
jyotipixolo <jyoti.pixolo@gmail.com>
2015-04-14 00:32:13 +0530

    array splited

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -8,11 +8,11 @@ angular.module('starter.controllers', [])
 .controller('DashCtrl', function ($scope) {
 
 
-    $scope.number = "";
-    $scope.change = function () {
-        input = $scope.number;
+	$scope.number = "";
+	$scope.change = function () {
+		input = $scope.number;
 
-    };
+	};
 
 
 
@@ -23,51 +23,58 @@ angular.module('starter.controllers', [])
 })
 
 .controller('ChatsCtrl', function ($scope, Chats) {
-    $scope.array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+	$scope.array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
-    $scope.jyoti = [];
-    for (var q = 0; q < $scope.array.length; q++) {
-        $scope.jyoti[q] = false;
-    };
-    var check = 0;
-    $scope.arr = [];
-
-
+	$scope.jyoti = [];
+	for (var q = 0; q < $scope.array.length; q++) {
+		$scope.jyoti[q] = false;
+	};
+	var check = 0;
+	$scope.arr = [];
 
 
-    $scope.magic = function (i) {
-        check++; //wrong way to do, this will become on every click, check length of $scope.arr instead
-        if (check < 6) {
-            $scope.jyoti[i] = true;
-            $scope.arr.push($scope.array[i]);
-
-        };
 
 
-    }
-    $scope.arr1=[];
-    for (var k = 0,c=0; k < $scope.array.length;k++) {
-        for (var j = 0; j < $scope.arr.length; j++) {
-            if ($scope.arr[j]== $scope.array[k]) {
-                c = o;
-                break;
-            }
-            else
-                c=1;
-        }
-    if(c==1)
-        $scope.arr1.push($scope.array[k]);
-    }
+	$scope.magic = function (i) {
+		check++; //wrong way to do, this will become on every click, check length of $scope.arr instead
+		if (check < 6) {
+			$scope.jyoti[i] = true;
+			$scope.arr.push($scope.array[i]);
+			if (check == 5)
+				$scope.spliting();
+		};
 
+
+
+
+
+
+	}
+	$scope.spliting = function () {
+		$scope.arr1 = [];
+		for (var k = 0, c = 0; k < $scope.array.length; k++) {
+			for (var j = 0; j < $scope.arr.length; j++) {
+				if ($scope.array[k] == $scope.arr[j]) {
+					c = 0;
+					break;
+				} else
+					c = 1;
+
+			}
+			if (c == 1)
+				$scope.arr1.push($scope.array[k]);
+		}
+
+	}
 
 })
 
 .controller('ChatDetailCtrl', function ($scope, $stateParams, Chats) {
-    $scope.chat = Chats.get($stateParams.chatId);
+	$scope.chat = Chats.get($stateParams.chatId);
 })
 
 .controller('AccountCtrl', function ($scope) {
-    $scope.settings = {
-        enableFriends: true
-    };
+	$scope.settings = {
+		enableFriends: true
+	};
 });
